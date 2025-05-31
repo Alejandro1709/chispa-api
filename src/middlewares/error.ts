8be57d2dbfc858159be02afe349b46f36f531ddb
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-05-31 13:09:46 -0500

    Add: Error handling for zod and mongooses casterror

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -5,13 +5,39 @@ import type {
   Response,
 } from 'express'
 
+import * as z from 'zod'
+
+const handleCastError = (res: Response, err: any) => {
+  const message = `Invalid ${err.path}: ${err.value}`
+  return res.status(400).json({ message })
+}
+
+const handleZodError = (res: Response, err: z.ZodError) => {
+  const errors = err.issues.map((issue) => ({
+    path: issue.path.join('.'),
+    message: issue.message,
+  }))
+
+  return res.status(400).json({ errors })
+}
+
 export const globalErrorHandler: ErrorRequestHandler = (
   err,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  console.log(err)
+  console.log(err.name)
+
+  if (err.name === 'CastError') {
+    handleCastError(res, err)
+    return
+  }
+
+  if (err.name === 'ZodError') {
+    handleZodError(res, err)
+    return
+  }
 
   res.status(500).json({ message: 'Internal Server Error' })
 }
