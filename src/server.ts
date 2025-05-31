2ffd8f9ec4e0e6902857f2f0f3c505d525d33ea4
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-05-31 10:06:38 -0500

    Add: morgan for dev loggin

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import express from 'express'
+import morgan from 'morgan'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 const app = express()
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
 const PORT = process.env.PORT || 2027
+const ENV = process.env.NODE_ENV
 
 app.listen(PORT, () => {
-  console.log(`Server is up and running on port ${PORT}`)
+  console.log(`Server is up and running on port ${PORT} in ${ENV} mode.`)
 })
