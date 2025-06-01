e5c7ab3f6fd34b180998686d7d1c3593648ef7f0
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-06-01 15:49:13 -0500

    Add: User schema and model

---
+++ src/models/User.ts
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose'
+
+export interface IUserDocument extends mongoose.Document {
+  name: string
+  username: string
+  email: string
+  password: string
+}
+
+const userSchema = new mongoose.Schema<IUserDocument>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 7,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const User = mongoose.model('User', userSchema)
+
+export default User
---
+++ src/schemas/userSchema.ts
@@ -0,0 +1,14 @@
+import * as z from 'zod'
+
+export const createUserSchema = z.object({
+  name: z.string({ required_error: 'Provide a name' }).min(1),
+  username: z.string({ required_error: 'Provide a username' }).min(1),
+  email: z
+    .string({ required_error: 'Provide an email' })
+    .email({ message: 'Provide a valid email address' }),
+  password: z
+    .string({ required_error: 'Provide a password' })
+    .min(7, 'Provide a password greater or equal to 7 characters'),
+})
+
+export type CreateUserInput = z.TypeOf<typeof createUserSchema>
